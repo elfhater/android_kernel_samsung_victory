3f071e2d03616db89aa512e3a48813bdbea6a440
Brian Beloshapka <bbelos@hotmail.com>
2012-01-01 18:42:48 -0600

    Add 600MHz, 900MHz, 1.1GHz, 1.2Gz, 1.3GHz, & 1.4GHz, thanks to nubecoder.

--- Kernel/drivers/cpufreq/cpufreq_interactivex.c
+++ Kernel/drivers/cpufreq/cpufreq_interactivex.c
@@ -28,6 +28,10 @@
 
 #include <asm/cputime.h>
 
+#ifdef CONFIG_MACH_VICTORY
+#include <mach/cpu-freq-v210.h>
+#endif
+
 static void (*pm_idle_old)(void);
 static atomic_t active_count = ATOMIC_INIT(0);
 
@@ -206,6 +210,9 @@ static void cpufreq_interactivex_freq_change_time_work(struct work_struct *work)
 	cpumask_t tmp_mask = work_cpumask;
 
 	for_each_cpu(cpu, tmp_mask) {
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+		printk(KERN_INFO "GOV:InteractiveX: early target_freq: %dMHz \n",target_freq/1000);
+#endif
 		if (!suspended && (target_freq >= freq_threshold || target_freq == policy->max) ) {
 			if (policy->cur < 400000) {
 			  // avoid quick jump from lowest to highest
@@ -215,19 +222,37 @@ static void cpufreq_interactivex_freq_change_time_work(struct work_struct *work)
 				cpumask_clear_cpu(cpu, &work_cpumask);
 				return;
 			}
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+			printk(KERN_INFO "GOV:InteractiveX: !suspended: using policy->max: %dMHz \n",policy->max/1000);
+#endif
 			__cpufreq_driver_target(policy, policy->max, CPUFREQ_RELATION_H);
 		} else {
 			if (!suspended) {
 				target_freq = cpufreq_interactivex_calc_freq(cpu);
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+				printk(KERN_INFO "GOV:InteractiveX: !suspended: using target_freq: %dMHz \n",target_freq/1000);
+#endif
 				__cpufreq_driver_target(policy, target_freq, CPUFREQ_RELATION_L);
 			} else {  // special care when suspended
 				if (target_freq > suspendfreq) {
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+					printk(KERN_INFO "GOV:InteractiveX: suspended: using suspendfreq: %dMHz \n",suspendfreq/1000);
+#endif
 					__cpufreq_driver_target(policy, suspendfreq, CPUFREQ_RELATION_H);
 				} else {
 					target_freq = cpufreq_interactivex_calc_freq(cpu);
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+					printk(KERN_INFO "GOV:InteractiveX: suspended: target_freq: %dMHz \n",target_freq/1000);
+#endif
 					if (target_freq < policy->cur) {
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+						printk(KERN_INFO "GOV:InteractiveX: suspended: using target_freq: %dMHz \n",target_freq/1000);
+#endif
 						__cpufreq_driver_target(policy, target_freq, CPUFREQ_RELATION_H);
 					}
+#ifdef CONFIG_DEBUG_SHADOWKERNEL
+					printk(KERN_INFO "GOV:InteractiveX: suspended: no change \n");
+#endif
 				}
 			}
 		}
